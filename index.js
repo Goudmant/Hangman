64d1a4ff488a190bd3d64bac058fe26198007412
Goudmant <yvangoudmant@gmail.com>
2020-11-02 15:00:57 +0100

    save

--- index.js
+++ index.js
@@ -26,9 +26,15 @@ function request()
 
 }
 
-function start ()
-{
-    fin = false;
+function débuter ()
+{   
+    if(la_touch == "")
+        return;
+
+    fin = false; nb_coups = 0; chaine_rangee="";
+    nb_passe = 0; nb_erreurs = 0; le_score = 10; 
+
+    suivant ();
 
 }
 
@@ -38,10 +44,37 @@ function init_calques ()
 
 }
 
-function next ()
+function suivant ()
 {
+    let tab_enigme; 
+    let nb_alea = Math.floor(Math.random()*nb_mots);
+    //test
+    //alert(nb_alea);
 
+    le_score = le_score - nb_erreurs/4;
 
+    document.getElementById("leScore").innerHTML = "Votre score : <strong>" + le_score " / 10</strong> - Mots restant : <strong>" + (10 - nb_passe) + "</strong>";
+    document.getElementById("lePendu").src = "img_pendu/";
+
+    nb_erreurs = 0; 
+    init_calques();
+
+    while(chaine_rangee.indexOf(" " + nb_alea + " ")>-1)
+    {
+        nb_alea = Math.floor(Math.random()*nb_mots);
+    }
+
+    chaine_rangee += " " + nb_alea + " ";
+    tab_enigme = tab_mots[nb_alea].split(' ');
+    le_mot = tab_enigme[0];
+    lindication = tab_enigme[1];
+    men_mot = le_mot.toUpperCase();
+    le_mot = le_mot.toUpperCase().replace(/[A-Z]/," ");
+    //test
+    //alert(indication + " " + men_mot );
+
+    document.getElementById("indication").innerHTML = "Indication : <br /><strong>" + lindication + "</strong>";
+    document.getElementById("leMot").innerHTML = le_mot;
 }
 
 function keybord(evenement)
